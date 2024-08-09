aa7ed894344fd6b4a018da5cffc80c6e977d08f2
mohd daniyal ashraf <daniyalashraf907@@example.com>
2024-08-09 18:30:28 +0530

    first commit

--- 08_reactRouter/src/componants/header/header.jsx
+++ 08_reactRouter/src/componants/header/header.jsx
@@ -61,7 +61,7 @@ export default function Header() {
                   to="/about/hello"
                   className={({ isActive }) =>
                     `${
-                      isActive ? "text-orange-700" : "tect-gery-700"
+                      isActive ? "text-orange-700" : "text-gray-700"
                     } block py-2 pr-4 pl-3 duration-200 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
                   }
                 >
