15519c6fcacb857649251d7a396e1147f515c7c9
mohd daniyal ashraf <daniyalashraf907@gmail.com>
2024-09-21 23:03:32 +0530

    save

---
+++ 13_megaproject/src/componants/authProtected.jsx
@@ -0,0 +1,23 @@
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+
+function authProtected({ children, authentication = true }) {
+  const navigate = useNavigate();
+  const [loader, setLoader] = useState(true);
+  const authstatus = useSelector((state) => state.authstatus);
+  useEffect(
+    () => {
+      if (authentication && authstatus !== authentication) {
+        navigate("/login");
+      } else if (!authentication && authstatus !== authentication) {
+        navigate("/");
+      }
+      setLoader(false);
+    },
+    { authstatus, authentication, navigate }
+  );
+  return loader ? <h1>Loading...</h1> : <div>{children}</div>;
+}
+
+export default authProtected;
